120eb63ed00489597445bdb118e68c7bc75a9b8b
parth sarin <psarin@stanford.edu>
2025-09-02 08:32:46 -0700

    train sh

---
+++ lm_parse_affiliations/train.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --job-name=grpo-affiliation-parsing
+#SBATCH -p batch
+#SBATCH --nodes=1
+#SBATCH -A marlowe-m000152-pm03
+#SBATCH --gpus=8
+#SBATCH --exclusive
+#SBATCH --time=24:00:00
+
+module load conda
+. "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate comet
+cd /scratch/m000152/comet/reconcile-pkp-beacon-journals-w-openalex-affiliation-metadata/lm_parse_affiliations
+
+accelerate launch train.py
